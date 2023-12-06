ac03f1c78588f329b06a794645ee3b1905973041
Deepak Kanshetti <kanshettid@gmail.com>
2023-12-06 22:44:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	num = 1;
+	while(n > 0){
+		num *= n;
+		n-=1;
+	}
+	var count = 0;
+	while( num % 10 == 0){
+		count += 1;
+		num /= 10;
+	}
+	return count;
 }
 
 const input = prompt("Enter a number");
